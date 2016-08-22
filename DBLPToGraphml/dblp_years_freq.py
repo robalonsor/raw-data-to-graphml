2148f1488dea3f91d5c2a6fea0d5aaf2c7e05aee
ga85rox <roberto.alonso@tum.de>
2016-08-22 10:19:19 +0200

    Some fixes in the DBLP script, optimization in the epinions script

--- DBLPToGraphml/dblp_years_freq.py
+++ DBLPToGraphml/dblp_years_freq.py
@@ -20,7 +20,7 @@ fOut = sys.argv[2]
 
 _file = open(fDBLP)
 
-conferences = ["conf/kdd/", "conf/icdm/", "conf/vldb/", "conf/icml/"]
+conferences = ["conf/kdd/","conf/icdm/", "conf/vldb/","conf/icml/","conf/www/","conf/pakdd/"]
 yearGraph1 = ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007"]
 yearGraph2 = ["2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"]
 frequency = 2
--- EpinionsToGraphml/eopinionsToGraphml.py
+++ EpinionsToGraphml/eopinionsToGraphml.py
@@ -2,7 +2,7 @@
 # a graphml format
 # First, consider the format of eopinions
 # 1 2 -1 979081200
-# first number is node ID
+# first number is node ID of a user
 # second number is node ID  (both nodes ID correspond to an edge)
 # the third value is the trust(+1) or distrust (-1)
 # the forth value is the timestamp
@@ -102,46 +102,6 @@ nodes = list(nodes)
 
 globalEdges = list(edges)
 
-#print "x"
-sNodes = ""
-for i in range(0,len(nodes)):
-	sNodes+='<node id="'+str(i)+'"/>\n'
-
-# first, att1 = positive trust  att2 = negative trust. 
-# s=""
-# for i in range(0,len(typeEdges)):
-# 	if edges[i] == -1:
-# 		#print "position ", i, "no longer valid"
-# 		continue
-# 	s+='<edge source="'+edges[i][0]+'" target="'+edges[i][1]+'" '
-# 	#print edges[i]
-# 	indices = findOcurrences(edges[i],edges,1)
-# 	#print indices
-# 	#print edges
-
-# 	posFlag = 0
-# 	negFlag = 0
-
-# 	for index in indices:
-# 		if typeEdges[index] == "1" and not posFlag:
-# 			s+='att1=10 '
-# 			posFlag = 1
-# 		if typeEdges[index] == "-1" and not negFlag:
-# 			s+='att2=20 '
-# 			negFlag = 1
-
-# 	s+='/>\n'
-# 	#print s
-# 	#exit()
-
-# f.close()
-
-# f = open("dataSetsEopinions/eopinions_PosToNeg.graphml","w")
-# f.write(sInitial+sNodes+s+sFinal)
-# f.close()
-# print edges
-
-
 
 # second, att1= positive trust in some period of time   att2 = positive trust wrt to the rest of time
 
@@ -157,16 +117,14 @@ for i in range(0,len(nodes)):
 
 
 s = ""
-#setTimestampG1 = ["979081200"] # set of timestamps to construct graph 1
-#setTimestampG1 = ["982623600","984092400","984178800","1006297200","1003701600"] # set of timestamps to construct graph 1
-#setTimestampG2 = ["1060639200","1044745200","1028930400","1021327200","1021240800"] # set of timestamps to construct graph 2
-setTimestampG1 = 982623600
-setTimestampG2 = 987638400
 
-tm = 987724800
-tm2 = 992995200
-setEdgesG1 = []
-setEdgesG2 = []
+setTimestampG1 = 979081200 
+setTimestampG2 = 991346400
+
+tm = 991432800
+tm2 = 1009753200
+setEdgesG1 = set()
+setEdgesG2 = set()
 
 for i in range(len(timestamps)):
 
@@ -177,23 +135,29 @@ for i in range(len(timestamps)):
 	#print timestamps[i], setEdgesG1
 
 	if int(timestamps[i]) >= setTimestampG1 and int(timestamps[i])<= setTimestampG2 and typeEdges[i] == "1": # if the timestamp is for graph 1
-		setEdgesG1.append(edges[i])
+		# setEdgesG1.append(edges[i])
+		setEdgesG1.add((edges[i][0],edges[i][1]))
+		continue
 		#print "adasd"
-	if int(timestamps[i]) >= tm and int(timestamps[i])<= tm2 and typeEdges[i] == "1": # if the timestamp is for graph 1
-		setEdgesG2.append(edges[i])
+	if int(timestamps[i]) >= tm and int(timestamps[i])<= tm2 and typeEdges[i] == "-1": # if the timestamp is for graph 1
+		# setEdgesG2.append(edges[i])
+		setEdgesG2.add((edges[i][0],edges[i][1]))
+
 	#if timestamps[i] in setTimestampG2 and typeEdges[i] == "1":
 	#	setEdgesG2.append(edges[i])
 
 print len(setEdgesG1)
 print len(setEdgesG2)
+
 #exit()
+
 
 nodes = set()
 
 while len(setEdgesG1) >0:
 	e = setEdgesG1.pop()
 	# print "->",e
-	# #print setEdgesG2
+	# print setEdgesG2
 	# print "-"
 	nodes.add(e[0])
 	nodes.add(e[1])
@@ -202,78 +166,52 @@ while len(setEdgesG1) >0:
 	posFlag = 0
 	negFlag = 0
 
-	j = 0
-	flagExit = 0
-	while len(setEdgesG2) > 0 and flagExit == 0:
-		# print setEdgesG2[j]
-		if [e[0],e[1]] == setEdgesG2[j]:
-			# print [e[0],e[1]]
-			if posFlag == 0:
-				s+='g2="20" '
-				posFlag = 1
-			setEdgesG2.pop(j)
-			j-=1
-		else:
-			if [e[1],e[0]] == setEdgesG2[j]:
-				# print [e[1],e[0]],"/"
-				if posFlag == 0:
-					s+='g2="20" '
-					posFlag = 1
-				setEdgesG2.pop(j)
-				j-=1
+	if (e[0],e[1]) in setEdgesG2 or (e[1],e[0]) in setEdgesG2:
+		s+='g2="20"'
+		setEdgesG2.discard((e[0],e[1]))
+		setEdgesG2.discard((e[1],e[0]))
+	# j = 0
+	# flagExit = 0
+	# while len(setEdgesG2) > 0 and flagExit == 0:
+	# 	# print setEdgesG2[j]
+	# 	if [e[0],e[1]] == setEdgesG2[j]:
+	# 		# print [e[0],e[1]]
+	# 		if posFlag == 0:
+	# 			s+='g2="20" '
+	# 			posFlag = 1
+	# 		setEdgesG2.pop(j)
+	# 		j-=1
+	# 	else:
+	# 		if [e[1],e[0]] == setEdgesG2[j]:
+	# 			# print [e[1],e[0]],"/"
+	# 			if posFlag == 0:
+	# 				s+='g2="20" '
+	# 				posFlag = 1
+	# 			setEdgesG2.pop(j)
+	# 			j-=1
 
-		j+=1
-		if j >= len(setEdgesG2):
-			flagExit=1
+	# 	j+=1
+	# 	if j >= len(setEdgesG2):
+	# 		flagExit=1
 	
 	s+='/>\n'
-	deleteOcurrences(e,setEdgesG1)
+	# deleteOcurrences(e,setEdgesG1)
 	# print setEdgesG2
 	# print "-end"
 	#exit()
 	pass
 
-# for i in range(len(setEdgesG1)):
-# 	if setEdgesG1[i] == -1:
-# 		#print "position ", i, "no longer valid"
-# 		continue
-# 	s+='<edge source="'+setEdgesG1[i][0]+'" target="'+setEdgesG1[i][1]+'" '
-# 	s+='g1=10 '
-# 	posFlag = 0
-# 	negFlag = 0
 
-# 	for j in range(len(setEdgesG2)):
-# 		if setEdgesG2[j] == -1:
-# 			continue
-# 		if [setEdgesG1[i][0],setEdgesG1[i][1]] == setEdgesG2[j]:
-# 			if not posFlag:
-# 				s+='g2=20 '
-# 				posFlag = 1
-# 			setEdgesG2[j] = -1
-
-# 		if [setEdgesG1[i][1],setEdgesG1[i][0]] == setEdgesG2[j]:
-# 			if not negFlag:
-# 				s+='g2=20 '
-# 				negFlag = 1
-# 			setEdgesG2[j] = -1
+# for i in range(len(setEdgesG2)):
+for item in setEdgesG2:
 	
-# 	s+='/>\n'
-# 	findOcurrences(setEdgesG1[i],setEdgesG1)
-
-# appending remaning edges
-
-#print "inserting remaning edges"
-
-for i in range(len(setEdgesG2)):
-	if setEdgesG2[i] == -1:
-		continue
-	s+='<edge source="'+setEdgesG2[i][0]+'" target="'+setEdgesG2[i][1]+'" '
+	s+='<edge source="'+item[0]+'" target="'+item[1]+'" '
 	s+='g2="20" '
 	s+='/>\n'
-	nodes.add(setEdgesG2[i][0])
-	nodes.add(setEdgesG2[i][1])
+	nodes.add(item[0])
+	nodes.add(item[1])
 	# print setEdgesG2[i][0],setEdgesG2[i][1]
-	findOcurrences(setEdgesG2[i],setEdgesG2,1)
+	# findOcurrences(setEdgesG2[i],setEdgesG2,1)
 
 #print s
 nodes = list(nodes)
@@ -283,7 +221,7 @@ for i in range(0,len(nodes)):
 	
 
 #f = open("dataSetsEopinions/eopinions_PosToPos_t1_t2.graphml","w")
-f = open("dataSetsEopinions/"+fOut+".graphml","w")
+f = open(fOut+".graphml","w")
 
 f.write(sInitial+sNodes+s+sFinal)
 f.close()
